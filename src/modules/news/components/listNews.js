081bfcaa7105fe269aa61121913557b09a9ff7a4
Reynaldo Maciel <reynaldommaciel@gmail.com>
2018-01-14 09:14:19 -0200

    Versão FlatList Reynaldo

--- src/modules/news/components/listNews.js
+++ src/modules/news/components/listNews.js
@@ -8,23 +8,25 @@ import styles from './styles/newslist.styles'
 export default class ListNews extends Component {
   render(){
     return(
-        <TouchableHighlight  onPress={ () => { Actions[consts.NEWS_INFO_SCENE]() } } underlayColor='transparent' >
+        <TouchableHighlight
+          onPress={ () => { Actions[consts.NEWS_INFO_SCENE]() } }
+          underlayColor='transparent' >
           <View style={ styles.newsList }>
 
-            <View style={ styles.viewList1 } >
+            <View style={ styles.viewList1 }>
               <Image style={ styles.imgCircle } source={{ uri: this.props.data.thumbnailUrl }} />
-              </View >
+            </View >
 
-              <View style={ styles.viewList2 } >
-                <Text style={ styles.textNews } >Descrição: { this.props.data.title }</Text>
-                <Text style={ styles.textNews } >Link: { this.props.data.url }</Text>
-              </View>
-
-              <View style={ styles.viewList3 }>
-                <Image style={ styles.imgArrow } source={require('../../../assets/imgs/img-arrow.png')} />
-              </View>
-
+            <View style={ styles.viewList2 } >
+              <Text style={ styles.textNews } >Descrição: { this.props.data.title }</Text>
+              <Text style={ styles.textNews } >Link: { this.props.data.url }</Text>
             </View>
+
+            <View style={ styles.viewList3 }>
+              <Image style={ styles.imgArrow } source={require('../../../assets/imgs/img-arrow.png')} />
+            </View>
+
+          </View>
         </TouchableHighlight>
     )
   }
