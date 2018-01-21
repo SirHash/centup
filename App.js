5f1b28252a4da25ffcca0f9bc533bcb99c450f03
higornunes <higor.fernando.nunes@gmail.com>
2018-01-21 20:54:45 -0200

    OneSignal implementado e testado

--- App.js
+++ App.js
@@ -6,16 +6,53 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Router from './Routers';
 
+import OneSignal from 'react-native-onesignal';
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore(reducers, {})}>
-        <Router/>
-      </Provider>
-    );
-  }
-};
+
+
+	componentWillMount() {
+	    OneSignal.addEventListener('received', this.onReceived);
+	    OneSignal.addEventListener('opened', this.onOpened);
+	    OneSignal.addEventListener('registered', this.onRegistered);
+	    OneSignal.addEventListener('ids', this.onIds);
+	}
+
+	componentWillUnmount() {
+	    OneSignal.removeEventListener('received', this.onReceived);
+	    OneSignal.removeEventListener('opened', this.onOpened);
+	    OneSignal.removeEventListener('registered', this.onRegistered);
+	    OneSignal.removeEventListener('ids', this.onIds);
+	}
+
+	onReceived(notification) {
+	    console.log("Notification received: ", notification);
+	}
+
+	onOpened(openResult) {
+	  console.log('Message: ', openResult.notification.payload.body);
+	  console.log('Data: ', openResult.notification.payload.additionalData);
+	  console.log('isActive: ', openResult.notification.isAppInFocus);
+	  console.log('openResult: ', openResult);
+	}
+
+	onRegistered(notifData) {
+	    console.log("Device had been registered for push notifications!", notifData);
+	}
+
+	onIds(device) {
+	console.log('Device info: ', device);
+	}
+
+	 render() {
+	    return (
+	      <Provider store={createStore(reducers, {})}>
+	        <Router/>
+	      </Provider>
+	    );
+	  }
+	};
 
 
 //🐴 Tenho que certeza que não me viu!!!, mas eu to aí.
